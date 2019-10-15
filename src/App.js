0ae31797e4f1be742da09a64cd1fd5df202626d2
pervezn <nidapervez2020@u.northwestern.edu>
2019-10-14 20:15:12 -0500

    Made UI Skeleton

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+const Nav = ({}) => (
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Study Stats</Navbar.Brand>
+  </Navbar>
+);
+
+const CurrClasses = ({}) => (
+  <Col>
+    <Card border="light">
+      <Card.Body>
+        <Card.Title><h1>Currently Enrolled Classes</h1></Card.Title>
+        <Card.Text>
+          <ListGroup variant="flush">
+            <ListGroup.Item><Button>EECS 340 - Homework 1</Button></ListGroup.Item>
+            <ListGroup.Item><Button>EECS 211 - Lab 1</Button></ListGroup.Item>
+          </ListGroup>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+const Reccomendations = ({}) => (
+  <Col>
+    <Card border="light">
+      <Card.Title>Reccomendations: </Card.Title>
+      <Card.Body>You should start 340 homework first.</Card.Body>
+    </Card>
+  </Col>
+);
+
+const Upcoming = ({}) => (
+  <Row>
+    <Col>
+      <Card border="light">
+        <Card.Title>Upcoming Week</Card.Title>
+      </Card>
+    </Col>
+    <Col>
+    </Col>
+  </Row>
+);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Nav/>
+      <Container>
+        <Row>
+          <CurrClasses/>
+          <Reccomendations/>
+        </Row>
+        <Upcoming/>
+      </Container>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
