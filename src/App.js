c6e3ff655d2b796f013ad8f9ed4dbfb0e19b0411
Irinel Bandas <ibandas@luc.edu>
2019-10-15 22:28:28 -0500

    takes new json data file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
@@ -21,24 +21,36 @@ const Nav = () => (
 const CurrClasses = ({state}) => {
    
   const [showLog, setShowLog] = useState(false);
-  const [logItem, setLogItem] = useState({title:"", assignment: "", median: 0});
+  const [logItem, setLogItem] = useState([{id: "", title: "", assignments: []}, {id: "", title: "", average_time_spent: "", completed: ""}]);
 
   const handleClose = () => setShowLog(false);
 
-  const handleSubmit = (currClass) => {
-    let newClasses = state.classes;
+  const handleSubmit = (currInfo) => {
+    console.log(state.classes);
+    let newClasses = [];
     let i = 0;
     for (i; i < state.classes.length; i += 1) {
-      if (_.isEqual(state.classes[i], currClass)) {
-        newClasses.splice(i, 1);
+      if (!_.isEqual(state.classes[i], currInfo[0])) {
+        newClasses.push(state.classes[i])
+      }
+      else {
+        let newAssignments = [];
+        let j = 0;
+        for (j; j < state.classes[i].assignments.length; j += 1) {
+          if (!_.isEqual(currInfo[1], state.classes[i].assignments[j])) {
+            newAssignments.push(state.classes[i].assignments[j])
+          }
+        }
+        newClasses.push({id: state.classes[i].id, title: state.classes[i].title, assignments: newAssignments});
       }
     }
+    console.log(newClasses);
     state.setClasses(newClasses);
     setShowLog(false);
   }
 
-  const handleShow = (currClass) => {
-    setLogItem(currClass);
+  const handleShow = (currClass, currAssignment) => {
+    setLogItem([currClass, currAssignment]);
     setShowLog(true);
   };
 
@@ -46,33 +58,32 @@ const CurrClasses = ({state}) => {
     <Col>
       <Card border="light">
         <Card.Body>
-          <Card.Title><h2>Upcoming Assignments</h2></Card.Title>
+          <Card.Title><h3>Upcoming Assignments</h3></Card.Title>
           <Card.Text>
             <ButtonGroup variant="flush">
               {state.classes.map(currClass => 
-                <React.Fragment key={currClass.title}>
-                <Button onClick={() => handleShow(currClass)}>{currClass.title} - {currClass.assignment}</Button>
+                currClass.assignments.map(currAssignment => 
+                  
+                <React.Fragment key={currAssignment.title}>
+                <Button onClick={() => handleShow(currClass, currAssignment)}>{currClass.title} - {currAssignment.title}</Button>
                 <br /> 
                 </React.Fragment>
-              )}
-            </ButtonGroup>
+              ))}
+            </ButtonGroup> 
             
             <Modal show={showLog} onHide={handleClose}>
               <Modal.Header closeButton>
-                <Modal.Title>Time Entry for {logItem.title} - {logItem.assignment}</Modal.Title>
+                <Modal.Title>Enter hours spent to complete this assignment:</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <Form>
                   <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>
-                      Total hours spent to complete this assignment:
-                    </Form.Label>
                     <Form.Control as="textarea" rows="2" />
                   </Form.Group>
                 </Form>
               </Modal.Body>
               <Modal.Footer>
-                <Button onClick={() => handleSubmit(logItem, state.classes)} variant="success">
+                <Button onClick={() => handleSubmit(logItem)} variant="success">
                   Submit
                 </Button>
               </Modal.Footer>
@@ -86,22 +97,24 @@ const CurrClasses = ({state}) => {
 
 const Recommendations = ({state}) => {
   let maxHours = 0;
-  let firstHW = "";
-  let firstClass = "";
-  console.log(state);
+  let cardText = "";
+  if (state.classes[0].assignments.length === 0) {
+    cardText = "Congrats! You have no more assignments."
+  }
   for (let i = 0; i < state.classes.length; i += 1) {
-    if (state.classes[i].median > maxHours) {
-      maxHours = state.classes[i].median
-      firstHW = state.classes[i].assignment
-      firstClass = state.classes[i].title
+    for (let j = 0; j < state.classes[i].assignments.length; j += 1) {
+      if (state.classes[i].assignments[j].average_time_spent > maxHours) {
+        maxHours = state.classes[i].assignments[j].average_time_spent;
+        cardText = "Past students have spent " + state.classes[i].assignments[j].average_time_spent + " hours on " + state.classes[i].title + " - " + state.classes[i].assignments[j].title + ". We recommend you start this one first!";
+      }
     }
   }
   return (
     <Col>
       <Card border="light">
         <Card.Body>
-          <Card.Title><h2>Recommendations:</h2></Card.Title>
-          <Card.Text>You should start {firstClass} - {firstHW} first.</Card.Text>
+          <Card.Title><h3>Recommendation:</h3></Card.Title>
+          <Card.Text>{cardText}</Card.Text>
         </Card.Body>
       </Card>
     </Col>
@@ -112,7 +125,7 @@ const Upcoming = () => (
     <Col>
       <Card border="light">
         <Card.Body>
-          <Card.Title><h2>Upcoming Week</h2></Card.Title>
+          <Card.Title><h3>Upcoming Week</h3></Card.Title>
         </Card.Body>
       </Card>
     </Col>
@@ -120,8 +133,21 @@ const Upcoming = () => (
 
 
 function App() {
-  const [classes, setClasses] = useState([{title: "", assignment: "", median: 0}]);
-  let classData = [
+  //const [classes, setClasses] = useState({title: "", assignment: [], median: 0});
+  const [classes, setClasses] = useState([{id: "", title: "", assignments: []}])
+  const url = '/data/assignments.json';
+
+  useEffect(() => {
+    const fetchClasses= async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setClasses(json.courses);
+    }
+    fetchClasses();
+  }, [])
+
+ /* let classData = [
     {
       title: "CS 340",
       assignment: "Homework 1",
@@ -132,21 +158,20 @@ function App() {
       assignment: "Lab 1",
       median: 2
     }
-  ];
-  if (classes[0]['title'] === "") {
-    setClasses(classData);
-  }
+  ];*/
+
+  //console.log(classes)
   
   return (
     <React.Fragment>
       <Nav/>
       <Container>
         <Row>
-          <CurrClasses key={classes[0].title} state={{classes, setClasses}}/>
-          <Recommendations state={{classes, setClasses}}/>
+          <CurrClasses key={classes.title} state={{classes, setClasses}}/>
+          <Upcoming />
         </Row>
         <Row>
-          <Upcoming />
+          <Recommendations state={{classes, setClasses}}/>
         </Row>
       </Container>
     </React.Fragment>
