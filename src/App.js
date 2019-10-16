ff2c4877de03c5ecd9fb2d00f90dce0bdb269a34
Ronit Basu <ronitbasu1234@gmail.com>
2019-10-15 21:26:20 -0500

    Google Graph API call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Chart } from "react-google-charts";
+
+const options = {
+  title: "This Week's Assignments",
+  vAxis: {
+    title: "Hours",
+    titleTextStyle: {
+      italic: false
+    }
+  }
+};
+
+const data = [
+  ['Assignment', 'Median Hours Spent', { role: 'style' }],
+  ['EECS 336 Assignment 1', 8, ''],
+  ['EECS 336 Assignment 2', 10, 'red'],
+  ['EECS 336 Assignment 3', 9, ''],
+  ['EECS 336 Assignment 4', 5, '']
+];
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"my-pretty-chart-container"}>
+
+      <Chart
+        chartType="ColumnChart"
+        data={data}
+        options={options}
+        width="80%"
+        height="400px"
+        legendToggle
+      />
     </div>
   );
 }
